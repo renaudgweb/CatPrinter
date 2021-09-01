abaf45f4ea756b48df10a40efd63f4bcf27a3f22
RenaudG <34276398+renaudgweb@users.noreply.github.com>
2021-09-02 01:37:35 +0200

    Update meteo.sh

--- app/meteo+/meteo.sh
+++ app/meteo+/meteo.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-weather LFPO -m -v > meteo.txt
-
 txt=$(<meteo.txt)
 
 curl --location -X POST --form text="$txt" --form 'font="ocr_b.ttf"' --form 'size="24"' --form 'feed="100"' 'localhost:5000'
