96728532307aa0d8267ecaaf1a8431fb230927ed
RenaudG <34276398+renaudgweb@users.noreply.github.com>
2021-09-03 18:09:35 +0200

    Update meteo.sh

--- app/meteo+/meteo.sh
+++ app/meteo+/meteo.sh
@@ -2,4 +2,4 @@
 
 txt=$(<meteo.txt)
 
-curl --location -X POST --form text="$txt" --form 'font="Lucida_Console_Regular.ttf"' --form 'size="22"' --form 'feed="100"' 'localhost:5000'
+curl --location -X POST --form text="$txt" --form 'font="Lucida_Console_Regular.ttf"' --form 'size="22"' 'localhost:5000'
