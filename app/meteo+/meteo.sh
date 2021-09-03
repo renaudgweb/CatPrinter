35dc7b661414509c62fe19a3f392012f1a084fc9
RenaudG <34276398+renaudgweb@users.noreply.github.com>
2021-09-03 17:57:07 +0200

    Update and rename meteo.sh to weather.sh

--- app/meteo+/meteo.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-txt=$(<meteo.txt)
-
-curl --location -X POST --form text="$txt" --form 'font="ocr_b.ttf"' --form 'size="22"' --form 'feed="100"' 'localhost:5000'
---
+++ app/meteo+/weather.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+txt=$(<weather.txt)
+
+curl --location -X POST --form text="$txt" --form 'font="Lucida_Console_Regular.ttf"' --form 'size="22"' --form 'feed="100"' 'localhost:5000'
