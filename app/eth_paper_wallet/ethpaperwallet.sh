b1bf3fb9a5f9903281f71528f265e843b6a7f54f
RenaudG <34276398+renaudgweb@users.noreply.github.com>
2021-11-28 00:33:52 +0100

    Add files via upload

---
+++ app/eth_paper_wallet/ethpaperwallet.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+python3 btcpaperwallet.py
+
+curl --location -X POST --form text="DO NOT LOSE OR SHARE THIS PRIVATE KEY !" 'localhost:5000'
+
+curl --location -X POST --form 'image=@/home/rengweb/Documents/catprinter/app/eth_paper_wallet/privatekey-qrcode.png' 'localhost:5000'
+
+txt1=$(<txt1.txt)
+curl --location -X POST --form text="$txt1" 'localhost:5000'
+
+curl --location -X POST --form text="PRIVATE KEY:" --form 'feed="100"' 'localhost:5000'
+
+curl --location -X POST --form text="------------------------------" --form 'feed="100"' 'localhost:5000'
+
+curl --location -X POST --form 'image=@/home/rengweb/Documents/catprinter/app/eth_paper_wallet/balancekey-qrcode.png' 'localhost:5000'
+
+curl --location -X POST --form text="Balance & transactions :" --form 'feed="100"' 'localhost:5000'
+
+curl --location -X POST --form text="------------------------------" --form 'feed="100"' 'localhost:5000'
+
+curl --location -X POST --form 'image=@/home/rengweb/Documents/catprinter/app/eth_paper_wallet/publicwalletqr.png' 'localhost:5000'
+
+txt2=$(<txt2.txt)
+curl --location -X POST --form text="$txt2" 'localhost:5000'
+
+curl --location -X POST --form text="PUBLIC KEY:" --form 'feed="100"' 'localhost:5000'
+
+curl --location -X POST --form 'image=@/home/rengweb/Documents/catprinter/app/eth_paper_wallet/eth_logo.png' --form 'feed="100"' 'localhost:5000'
+
+sudo rm privatekey-qrcode.png
+sudo rm balancekey-qrcode.png
+sudo rm publicwalletqr.png
+echo "" > txt1.txt
+echo "" > txt2.txt
