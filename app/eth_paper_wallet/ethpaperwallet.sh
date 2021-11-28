0a2326eae24f9a4570ef620714e51e45d54e34b5
RenaudG <34276398+renaudgweb@users.noreply.github.com>
2021-11-28 01:26:07 +0100

    Update ethpaperwallet.sh

--- app/eth_paper_wallet/ethpaperwallet.sh
+++ app/eth_paper_wallet/ethpaperwallet.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-python3 btcpaperwallet.py
+python3 ethpaperwallet.py
 
 curl --location -X POST --form text="DO NOT LOSE OR SHARE THIS PRIVATE KEY !" 'localhost:5000'
 
-curl --location -X POST --form 'image=@/home/rengweb/Documents/catprinter/app/eth_paper_wallet/privatekey-qrcode.png' 'localhost:5000'
+curl --location -X POST --form 'image=@/home/yourpath/Documents/catprinter/app/eth_paper_wallet/privatekey-qrcode.png' 'localhost:5000'
 
 txt1=$(<txt1.txt)
 curl --location -X POST --form text="$txt1" 'localhost:5000'
@@ -13,20 +13,22 @@ curl --location -X POST --form text="PRIVATE KEY:" --form 'feed="100"' 'localhos
 
 curl --location -X POST --form text="------------------------------" --form 'feed="100"' 'localhost:5000'
 
-curl --location -X POST --form 'image=@/home/rengweb/Documents/catprinter/app/eth_paper_wallet/balancekey-qrcode.png' 'localhost:5000'
+curl --location -X POST --form 'image=@/home/yourpath/Documents/catprinter/app/eth_paper_wallet/balancekey-qrcode.png' 'localhost:5000'
+
+curl --location -X POST --form text="(etherscan.io)" 'localhost:5000'
 
 curl --location -X POST --form text="Balance & transactions :" --form 'feed="100"' 'localhost:5000'
 
 curl --location -X POST --form text="------------------------------" --form 'feed="100"' 'localhost:5000'
 
-curl --location -X POST --form 'image=@/home/rengweb/Documents/catprinter/app/eth_paper_wallet/publicwalletqr.png' 'localhost:5000'
+curl --location -X POST --form 'image=@/home/yourpath/Documents/catprinter/app/eth_paper_wallet/publicwalletqr.png' 'localhost:5000'
 
 txt2=$(<txt2.txt)
 curl --location -X POST --form text="$txt2" 'localhost:5000'
 
 curl --location -X POST --form text="PUBLIC KEY:" --form 'feed="100"' 'localhost:5000'
 
-curl --location -X POST --form 'image=@/home/rengweb/Documents/catprinter/app/eth_paper_wallet/eth_logo.png' --form 'feed="100"' 'localhost:5000'
+curl --location -X POST --form 'image=@/home/yourpath/Documents/catprinter/app/eth_paper_wallet/eth_logo.png' --form 'feed="100"' 'localhost:5000'
 
 sudo rm privatekey-qrcode.png
 sudo rm balancekey-qrcode.png
