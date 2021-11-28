97b014c3e99c8d29dca9a30141c66915512a9337
RenaudG <34276398+renaudgweb@users.noreply.github.com>
2021-11-28 03:22:58 +0100

    Update ethpaperwallet.sh

--- app/eth_paper_wallet/ethpaperwallet.sh
+++ app/eth_paper_wallet/ethpaperwallet.sh
@@ -2,6 +2,10 @@
 
 python3 ethpaperwallet.py
 
+sed -i 's/.\{30\}/& /g' txt1.txt
+
+sed -i 's/.\{30\}/& /g' txt2.txt
+
 curl --location -X POST --form text="DO NOT LOSE OR SHARE THIS PRIVATE KEY !" 'localhost:5000'
 
 curl --location -X POST --form 'image=@/home/yourpath/Documents/catprinter/app/eth_paper_wallet/privatekey-qrcode.png' 'localhost:5000'
@@ -9,7 +13,7 @@ curl --location -X POST --form 'image=@/home/yourpath/Documents/catprinter/app/e
 txt1=$(<txt1.txt)
 curl --location -X POST --form text="$txt1" 'localhost:5000'
 
-curl --location -X POST --form text="PRIVATE KEY:" --form 'feed="100"' 'localhost:5000'
+curl --location -X POST --form text="PRIVATE KEY:" 'localhost:5000'
 
 curl --location -X POST --form text="------------------------------" --form 'feed="100"' 'localhost:5000'
 
