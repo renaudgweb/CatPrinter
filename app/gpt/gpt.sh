ce43e5d39f7a3234997803f4ccf06149d6869e11
RenaudG <34276398+renaudgweb@users.noreply.github.com>
2023-04-04 15:24:22 +0200

    Update gpt.sh

--- app/gpt/gpt.sh
+++ app/gpt/gpt.sh
@@ -2,4 +2,4 @@
 
 TXT=$(<gpt.txt)
 
-curl --location -X POST --form text="$TXT" --form 'font="VG5000-Regular_web.ttf"' --form 'size="26"' --form 'feed="100"' 'localhost:5000'
+curl --location -X POST --form text="$TXT" --form 'font="VG5000-Regular_web.ttf"' --form 'size="30"' --form 'feed="100"' 'localhost:5000'
