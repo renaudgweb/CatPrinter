c2d0ce5091e72810ef0a060e36b067d7752981bc
RenaudG <34276398+renaudgweb@users.noreply.github.com>
2024-01-09 18:00:14 +0100

    add config file

--- install.sh
+++ install.sh
@@ -51,8 +51,10 @@ default_install() {
     nextcloud_talk_channel_id="[Nextcloud_Talk_api]\nNEXTCLOUD_TALK_CHANNEL_ID = CHANNEL-ID-HERE"
     openweather_api_key="[Openweather_api]\nOPENWEATHER_API_KEY = APIKEY-HERE"
 
-    echo -e "# Config file\n\n" > "$config_file_ini"
-    echo -e "$home_path\n\n$telegram_bot_token\n\n$openai_api_key\n\n$nextcloud_talk_channel_id\n\n$openweather_api_key" > "$config_file_ini"
+    echo -e "# Config file\n\n$home_path" \
+    "\n$telegram_bot_token\n$openai_api_key" \
+    "\n$nextcloud_talk_channel_id\n$openweather_api_key" > "$config_file_ini"
+
     echo -e "HOME_PATH=\"$current_path\"\nexport HOME_PATH" > "$config_file_sh"
     printf "Paths are defined successfully ✔️\n"
 
